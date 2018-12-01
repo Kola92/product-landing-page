98c137e4a145fb868cf095f905b19dacdace9787
Adekola Olawale <rolawale92@gmail.com>
2018-12-01 16:04:15 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '67%';
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('menu').style.position = 'relative';
+    document.getElementById('menu').style.top = '7px';
+    document.getElementById('menu').style.left = '57px';
+    document.getElementById('menu').style.backgroundColor = 'hsla(0,0%,0%,0.7)';
+    document.getElementById('menu').style.width = '67%';
+    document.getElementById('menu').style.paddingRight = '1.7em';
+}
